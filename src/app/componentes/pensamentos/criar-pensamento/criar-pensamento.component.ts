9c59058e3a2fcc32b1e5ac8bcc090d5d4032a450
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-06-13 02:15:14 -0300

    Ajustes de funções no forms

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo:''
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado")
+  }
+  cancelar() {
+    alert("Ação cancelada")
+  }
+}
