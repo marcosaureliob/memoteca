6bf4917ddda2cf448a97f3fb18aab99653feb309
Marcos Aurelio <marcosaureliobrazdossantos@gmail.com>
2024-08-22 20:16:15 -0300

    feat: finalização do projeto e construção de funcionalidades

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,21 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    { conteudo: 'Comunicação entre componentes',
-    autoria: 'Angular',
-    modelo: 'modelo3'},
-    { conteudo: 'Informações para o componente filho',
-    autoria: 'Componente pai',
-    modelo: 'modelo1'},
-    { conteudo: 'Morbi leo mi, nonummy eget, tristique non, rhoncus non, leo. Nullam faucibus mi quis velit. Integer in sapien. Fusce tellus odio, dapibus id, fermentum quis, suscipit id, erat. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat. Pellentesque sapien. Cras elementum. Nulla pulvinar eleifend sem. Cum sociis natoque penatibus et magnis dis',
-    autoria: 'Lorem',
-    modelo: 'modelo1'},
-  ];
+  listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
   }
 
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  @Input() pensamento = {
+  @Input() pensamento: Pensamento = {
+    id: 0,
     conteudo: 'I love Angular',
-    autoria: 'Marcos',
+    autoria: 'Nay',
     modelo: 'modelo3'
   }
 
@@ -19,9 +21,10 @@ export class PensamentoComponent implements OnInit {
   }
 
   larguraPensamento(): string {
-    if (this.pensamento.conteudo.length >= 256) {
+    if(this.pensamento.conteudo.length >= 256) {
       return 'pensamento-g'
     }
     return 'pensamento-p'
   }
+
 }
